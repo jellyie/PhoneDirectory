8515a6639ff03e2fe4df1a47fd3565d2c3958f74
Irene Ong <okirynn@gmail.com>
2018-10-21 10:03:36 -0400

    Added documentation

--- src/Phonedir.java
+++ src/Phonedir.java
@@ -8,34 +8,58 @@ public class Phonedir extends Customer{
 	LinkedList<Customer> list;
 	Customer current;
 	
+	/**
+	 * Default constructor that initializes LinkedList.
+	 */
 	public Phonedir(){
 		list = new LinkedList<Customer>();
 	}
 	
+	/**
+	 * A method that adds a Customer object to LinkedList and sets the current customer to this object.
+	 * @param  c    Customer object to be added
+	 */
 	public void addCustomer(Customer c) {
 		list.add(c);
 		current = c;
 		this.sort();
 	}
 	
+	/**
+	 * A method that changes the first name of the current customer to string parameter.
+	 * @param  f    String that represents the new first name
+	 */
 	public void changeFirstName(String f) {
 		current.setFirstName(f);
 		list.set(list.indexOf(current), current);
 		this.sort();
 	}
 	
+	/**
+	 * A method that changes the last name of the current customer to string parameter.
+	 * @param  l    String that represents the new last name
+	 */
 	public void changeLastName(String l) {
 		current.setLastName(l);
 		list.set(list.indexOf(current), current);
 		this.sort();
 	}
 	
+	/**
+	 * A method that changes the phone number of the current customer to string parameter.
+	 * @param  p    String that represents the new phone number
+	 */
 	public void changePhoneNumber(String p) {
 		current.setPhoneNumber(p);
 		list.set(list.indexOf(current), current);
 		this.sort();
 	}
 	
+	/**
+	 * A method that selects a customer record as the current record. 
+	 * @param  first_name    String that represents the customer's first name
+	 * @param  last_name     String that represents the customer's last name
+	 */
 	public void setCurrentRecord(String first_name, String last_name) {
 		int size = list.size()-1;
 			while(size >= 0) {
@@ -46,6 +70,12 @@ public class Phonedir extends Customer{
 			}
 	}
 	
+	/**
+	 * A method that determines whether a specified customer record exists in this directory.  
+	 * @param  first_name    String that represents the customer's first name
+	 * @param  last_name     String that represents the customer's last name
+	 * @return  			 true if customer record exists in this directory
+	 */
 	public boolean recordExists(String first_name, String last_name) {
 		int size = list.size()-1;
 		boolean exists = false;
@@ -61,6 +91,10 @@ public class Phonedir extends Customer{
 		return exists;
 	}
 	
+	/**
+	 * A method that determines whether there is current record or not.   
+	 * @return  			 true if a current customer record is selected
+	 */
 	public boolean currentRecordExists() {
 		boolean exists = false;
 		if(current != null) {
@@ -71,6 +105,9 @@ public class Phonedir extends Customer{
 		return exists;
 	}
 	
+	/**
+	 * A method that removes the current record.  
+	 */
 	public void deleteCurrentRecord() {
 		System.out.println("\nDeleted: "+ current.toString());
 		list.remove(current);
@@ -78,10 +115,16 @@ public class Phonedir extends Customer{
 		this.sort();
 	}
 	
+	/**
+	 * A method that sorts the directory.  
+	 */
 	public void sort() {
 		Collections.sort(list, new Customer());
 	}
 	
+	/**
+	 * A method that prints the entire directory to console.  
+	 */
 	public void displayAll() {
 		Iterator<Customer> itr = list.iterator();
 		String first_line = "\nFirst Name   Last Name   Phone Number";
@@ -94,6 +137,9 @@ public class Phonedir extends Customer{
 		}
 	}
 	
+	/**
+	 * A method that prints the current customer information to console.  
+	 */
 	public void displayCurrent() {
 		if(currentRecordExists()) {
 			System.out.print("\nThe current record is: " + current.toString());
